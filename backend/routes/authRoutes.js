603488fe34280ecf03e495ae3bca5814c1856b77
Vishal Dwivedy <gautamdwivedy930@gmail.com>
2025-03-28 21:36:45 +0530

    'again backend'

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/authController");
+const { register, login, getUser } = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post('/register', register);
+router.post('/login', login);
+router.get('/me', authMiddleware, getUser);
 
 module.exports = router;
